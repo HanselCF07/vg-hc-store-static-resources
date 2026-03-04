fb42df867de58a04049b2e4269d3688a72d70cd6
Hansel Castro <44871366+HanselCF07@users.noreply.github.com>
2026-03-04 16:53:05 -0500

    Update routes

--- src/routes/gridFSRoutes.js
+++ src/routes/gridFSRoutes.js
@@ -6,8 +6,8 @@ const uploadMulter = require("../middlewares/uploadMulter");
 const resourceController = require("../controllers/gridFSController");
 
 router.post("/upload", authMiddleware(["Full Access", "Management Access"]), uploadMulter.single("file"), resourceController.uploadFile);
-router.get("/:product_id", authMiddleware(["Full Access", "Basic Access", "Management Access"]), resourceController.getFilesByProductId);
-router.get("/file/:id", authMiddleware(["Full Access", "Basic Access", "Management Access"]), resourceController.getFileById);
+router.get("/:product_id", resourceController.getFilesByProductId);
+router.get("/file/:id", resourceController.getFileById);
 router.delete("/file/:id", authMiddleware(["Full Access", "Management Access"]), resourceController.deleteFileById);
 
 module.exports = router;
